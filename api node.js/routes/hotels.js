237b5cf92a5389057e26a1c69e31c3d5e677eff2
jess-bk <74265740+jess-bk@users.noreply.github.com>
2022-11-28 12:15:51 +0000

    Booking App Back End API Completed v1

---
+++ api node.js/routes/hotels.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+
+const router = express.Router();
+
+router.post("/", createHotel);
+router.put("/:id", updateHotel);
+router.delete("/:id", deleteHotel);
+
+router.get("/find/:id", getHotel);
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router;
